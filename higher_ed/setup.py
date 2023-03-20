599b447a8c038ef448478657996961f0ffd8d230
keb101 <butlerk26@gmail.com>
2023-03-20 16:27:24 -0400

    first commit

---
+++ higher_ed/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='possible 2nd capstone project',
+    author='Kelly Butler',
+    license='',
+)
